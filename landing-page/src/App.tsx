ddf8017212527dd6cd1d70f7c876cf63544b4ad3
Dalmas Ogembo <dalmasogembo@gmail.com>
2024-07-17 01:53:43 +0300

    Update landing page, change default themobject

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -30,16 +30,16 @@ function App() {
   const snap = useSnapshot(landingDb)
 
   const defaultThemeObj = {
-    "r": "20px",
-    "textColor": "black",
+    "textColor": "rgba(255, 255, 255, 1)",
     "headerFooterBg": "rgba(0, 0, 0, 0.1)",
-    "backgroundColor": "#ffdfa8",
+    "backgroundColor": "rgba(35, 14, 99, 1)",
     "fontFamily": "Space Grotesk, sans-serif",
     "searchBackground": "rgba(0, 0, 0, 0.1)",
     "searchColor": "black",
     "searchBorderColor": "rgba(14, 6, 46, 0)",
     "searchFocusBorderColor": "#845ef7",
-    "primaryColor": "#845ef7",
+    "primaryColor": "rgba(174, 0, 255, 1)",
+    "r": "20px"
   }
   return (
     <BrowserRouter>
--- landing-page/src/simpledb.ts
+++ landing-page/src/simpledb.ts
@@ -1,3 +1,16 @@
 import { proxy } from 'valtio'
 
-export const landingDb = proxy({ defaultThemeObj: null } as any)
+export const landingDb = proxy({
+    defaultThemeObj: {
+        "textColor": "rgba(255, 255, 255, 1)",
+        "headerFooterBg": "rgba(0, 0, 0, 0.1)",
+        "backgroundColor": "rgba(35, 14, 99, 1)",
+        "fontFamily": "Space Grotesk, sans-serif",
+        "searchBackground": "rgba(0, 0, 0, 0.1)",
+        "searchColor": "black",
+        "searchBorderColor": "rgba(14, 6, 46, 0)",
+        "searchFocusBorderColor": "#845ef7",
+        "primaryColor": "rgba(174, 0, 255, 1)",
+        "r": "20px"
+    }
+} as any)
